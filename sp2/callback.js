2954e7c104485b385abc20afc54e0d9c6d824003
Daniel D <daniel@dittenhafer.net>
2026-08-05 13:17:44 -0700

    Update callback.js

--- sp2/callback.js
+++ sp2/callback.js
@@ -1,16 +1,31 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOMContentLoaded");
 
-  // Get query string tuples
-  const urlParams = new URLSearchParams(window.location.search);
+  try {
+    // Get query string tuples
+    const urlParams = new URLSearchParams(window.location.search);
+    const cbData = JSON.parse(urlParams.get("result");
 
-  // {"status":500,"proposal_names":[],"google_merchant_id":"5423845892","provider_account_id":"abc-xyz-123","timestamp":"2026-08-05T19:59:02.399Z"}
-  // Get values
+    // {"status":500,"proposal_names":[],"google_merchant_id":"5423845892","provider_account_id":"abc-xyz-123","timestamp":"2026-08-05T19:59:02.399Z"}
+    let node = document.getElementById("messageOutput");
+    let eventData = ""; 
+    eventData += "status: " + status;
+    eventData += "<br />proposal_names: " + proposal_names;
+    eventData += "<br />google_merchant_id: " + google_merchant_id;
+    eventData += "<br />timestamp: " + timestamp;
+    node.textContent = eventData;
+  }
+  catch (e) {
+    console.error("Failed to parse callback data into JSON:", e.message);
+  }
+});
+
+function alternateApproach()
+{
+  const urlParams = new URLSearchParams(window.location.search);
   const status = urlParams.get('status');
   const proposal_names = urlParams.get('proposal_names');
   const google_merchant_id = urlParams.get('google_merchant_id');
   const provider_account_id = urlParams.get('provider_account_id');
   const timestamp = urlParams.get('timestamp');
-
-  
-});
+}
