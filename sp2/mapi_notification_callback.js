104ad49d50bd47f733279d6c65d57524954d7815
Daniel D <daniel@dittenhafer.net>
2026-08-05 16:26:19 -0700

    Implement DOMContentLoaded event listener
    Add event listener for DOMContentLoaded to handle callback.

---
+++ sp2/mapi_notification_callback.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOMContentLoaded");
+
+  try {
+    // TODO: Get POST data and parse out... 
+  }
+  catch (e) {
+    console.error("Failed to parse callback data into JSON:", e.message);
+  }
+});
