876c27235b587821e7dc7f61fe0ca2c0ba071eb6
Daniel D <daniel@dittenhafer.net>
2026-08-10 14:01:30 -0700

    Retrieve provider_merchant_id and update output
    Added retrieval of provider_merchant_id from URL parameters and included it in the output.

--- sp2/callback.js
+++ sp2/callback.js
@@ -17,7 +17,8 @@ function processResults()
   const status = urlParams.get('status');
   const proposal_names = urlParams.get('proposal_names');
   const google_merchant_id = urlParams.get('google_merchant_id');
-  const provider_account_id = urlParams.get('provider_account_id');
+  const provider_account_id = urlParams.get('provider_account_id'); // legacy / alpah
+  const provider_merchant_id = urlParams.get('provider_merchant_id'); // beta / GA
   const timestamp = urlParams.get('timestamp');
 
   // {"status":500,"proposal_names":[],"google_merchant_id":"5423845892","provider_account_id":"abc-xyz-123","timestamp":"2026-08-05T19:59:02.399Z"}
@@ -27,6 +28,7 @@ function processResults()
   eventData += createTableRow("proposal_names", proposal_names);
   eventData += createTableRow("google_merchant_id", google_merchant_id); 
   eventData += createTableRow("provider_account_id", provider_account_id);
+  eventData += createTableRow("provider_merchant_id", provider_merchant_id);
   eventData += createTableRow("timestamp", timestamp);
   node.innerHTML = eventData;
   
