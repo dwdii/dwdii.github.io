f61a5549fddc544be1b91a90f312d8607fe3a1bc
Daniel D <daniel@dittenhafer.net>
2026-08-13 18:41:36 -0700

    Update callback.js

--- sp2/callback.js
+++ sp2/callback.js
@@ -15,11 +15,11 @@ function processResults()
 {
   const urlParams = new URLSearchParams(window.location.search);
   const status = urlParams.get('status');
-  const proposal_names = urlParams.get('proposal_names');
-  const google_merchant_id = urlParams.get('google_merchant_id');
-  const provider_account_id = urlParams.get('provider_account_id'); // legacy / alpha
-  const provider_merchant_id = urlParams.get('provider_merchant_id'); // beta / GA
-  const timestamp = urlParams.get('timestamp');
+  // const proposal_names = urlParams.get('proposal_names');
+  // const google_merchant_id = urlParams.get('google_merchant_id');
+  // const provider_account_id = urlParams.get('provider_account_id'); // legacy / alpha
+  // const provider_merchant_id = urlParams.get('provider_merchant_id'); // beta / GA
+  // const timestamp = urlParams.get('timestamp');
 
   let nodeString = document.getElementById("humanMessage");
   let humanMessage = "Unknown result";
@@ -35,16 +35,20 @@ function processResults()
   // {"status":500,"proposal_names":[],"google_merchant_id":"5423845892","provider_account_id":"abc-xyz-123","timestamp":"2026-08-05T19:59:02.399Z"}
   let node = document.getElementById("messageOutput");
   let eventData = ""; 
-  eventData += createTableRow("status", status);
-  eventData += createTableRow("proposal_names", proposal_names);
-  eventData += createTableRow("google_merchant_id", google_merchant_id); 
-  if(null != provider_account_id) {
-    eventData += createTableRow("provider_account_id (alpha)", provider_account_id);
+  for (const key of urlParams.keys()) {
+    eventData += createTableRow(key, urlParams.get(key));
   }
-  if(null != provider_merchant_id) {
-    eventData += createTableRow("provider_merchant_id", provider_merchant_id);
-  }
-  eventData += createTableRow("timestamp", timestamp);
+  
+  // eventData += createTableRow("proposal_names", proposal_names);
+  // eventData += createTableRow("google_merchant_id", google_merchant_id); 
+  // if(null != provider_account_id) {
+  //   eventData += createTableRow("provider_account_id (alpha)", provider_account_id);
+  // }
+  // if(null != provider_merchant_id) {
+  //   eventData += createTableRow("provider_merchant_id", provider_merchant_id);
+  // }
+  // eventData += createTableRow("timestamp", timestamp);
+  
   node.innerHTML = eventData;
   
 }
